d39293356a013fdf4e54b6f88197c003b47bbb04
Rominaraya <rarayaa@utem.cl>
2025-09-25 10:13:30 -0300

    Agregar logging en ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,12 +1,24 @@
 import os
 import requests
 import json
+import logging
 from pathlib import Path
 from datetime import datetime
 
+LOGS_DIR = Path(__file__).resolve().parents[1] / "logs"
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(LOGS_DIR / "pipeline.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
 API_URL = "https://api.ebird.org/v2/data/obs/{regionCode}/recent"
 API_KEY = os.getenv("EBIRD_API_KEY")
-
 
 RAW_DATA_DIR = Path(__file__).resolve().parents[1] / "data" / "raw"
 RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
@@ -30,25 +42,34 @@ def fetch_recent_observations(region_code="CL-RM", max_results=50, back_days=7, 
         "sppLocale": locale
     }
 
+    logging.info(f"Solicitando datos de la región {region_code} con {max_results} resultados máximos")
+
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
+        logging.info("Datos recibidos correctamente desde la API")
         return response.json()
     else:
+        logging.error(f"Error {response.status_code}: {response.text}")
         raise Exception(f"Error {response.status_code}: {response.text}")
 
 
 def save_raw_data(data, region_code="CL-RM"):
+
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = RAW_DATA_DIR / f"ebird_{region_code}_{timestamp}.json"
 
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-    print(f"Datos guardados en {filename}")
+    logging.info(f"Datos guardados en {filename}")
 
 
 if __name__ == "__main__":
-    observations = fetch_recent_observations(region_code="CL-RM", max_results=20)
-    save_raw_data(observations, region_code="CL-RM")
+    try:
+        observations = fetch_recent_observations(region_code="CL-RM", max_results=20)
+        save_raw_data(observations, region_code="CL-RM")
+    except Exception:
+        logging.exception("Fallo en la ingesta de datos")
+
 
